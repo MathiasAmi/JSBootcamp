cfc0c72c765cd7c7f7d916186d83dedde029da55
Mathiasami <math35i0@edu.easj.dk>
2019-09-25 21:38:47 +0200

    first exercise

---
+++ JSBootcamp.js
@@ -0,0 +1,10 @@
+function sum(n) {
+    var sum = 0;
+    for (var i =0; 1 <= arguments.length; i++){
+        sum += arguments[i];
+    }
+    return sum;
+  }
+  sum(3); // => 6
+  sum(4); // => 10
+  sum(5); // => 15
